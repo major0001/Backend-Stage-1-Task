96796afe081d21748e51457198d7f07ab42ca82f
major0001 <alexmutune746@gmail.com>
2022-10-27 06:23:53 -0700

    API

--- Backend-Stage-1-Task.py
+++ Backend-Stage-1-Task.py
@@ -1,2 +1,2 @@
-dict1 = {'slackUsername': Alex Mutune, 'backend': true, 'age': 23, 'bio': "Tech enthusiast who is always happy to do hard things" }
+dict1 == {'slackUsername': Alex Mutune, 'backend': true, 'age': 23, 'bio': "Tech enthusiast who is always happy to do hard things" }
 print(dict1)
